e384eec5023d0c52f092e0cfd9ccf04cae173086
ha-se <hasebe.2006@gmail.com>
2022-05-22 16:01:38 +0900

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,9 @@ module.exports = {
     title: `Learn to invest`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,19 @@
 import * as React from "react"
 import Layout from "./components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Zhoudongyu"
+        src="https://i.mydramalist.com/EoLeb_5c.jpg"
+      />
+      <StaticImage
+        alt="Masizhun"
+        src="../images/7d5d1bbc9c71c3446996e07cecd2a719.jpg"
+      />
     </Layout>
       
   )
